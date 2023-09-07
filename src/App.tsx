d67b7c2b8004f990b1103fa0f04d3bdafa864610
Aleksey Loktev <a.a.loktev@yandex.ru>
2023-09-07 11:20:43 +0300

    feat: removed app.scss

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import { store } from "./store/store";
 import { Provider } from "react-redux";
-import "./App.scss";
 import { GlobalStyle } from "./assets/styles/GlobalStyle";
 
 function App() {
