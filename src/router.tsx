ffc579367f92d2fe17a0240d92b8fe48b9939ee7
Aleksey Loktev <a.a.loktev@yandex.ru>
2023-09-09 15:44:51 +0300

    fix: routing

--- src/router.tsx
+++ src/router.tsx
@@ -5,28 +5,31 @@ import { TodoListPage } from "./pages/TodoListPage";
 import { ViewListItemPage } from "./pages/ViewListItemPage";
 import { NotFountPage } from "./pages/404/404";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    errorElement: <NotFountPage />,
-    children: [
-      {
-        path: "/",
-        element: <TodoListPage />,
-      },
-      {
-        path: "/list",
-        element: <ViewListPage />,
-      },
-      {
-        path: "/list/:id",
-        element: <ViewListItemPage />,
-      },
-    ],
-  },
-  {
-    path: "*",
-    element: <NotFountPage />,
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <NotFountPage />,
+      children: [
+        {
+          path: "/",
+          element: <TodoListPage />,
+        },
+        {
+          path: "/list",
+          element: <ViewListPage />,
+        },
+        {
+          path: "/list/:id",
+          element: <ViewListItemPage />,
+        },
+      ],
+    },
+    {
+      path: "*",
+      element: <NotFountPage />,
+    },
+  ],
+  { basename: "/app/" }
+);
